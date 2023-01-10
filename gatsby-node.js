97cfb6aabd614b2069cde792da64cc2a2f651d19
Maksim Amelchenko <m.amelchenko@gmail.com>
2023-01-10 22:21:59 +0100

    fix: inline only assets under 1K size

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+  const config = getConfig();
+
+  for (const { test, use } of config.module.rules) {
+    if (String(test).includes('.svg')) {
+      for (const rule of use) {
+        if (rule.loader.includes('/url-loader/')) {
+          rule.options.limit = 1024;
+        }
+      }
+    }
+  }
+
+  actions.replaceWebpackConfig(config);
+};
