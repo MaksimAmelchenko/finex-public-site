c1fe0f2e46f733b0f71c90cd42e51a299a04df83
Maksim Amelchenko <m.amelchenko@gmail.com>
2023-06-12 14:09:25 +0200

    fix: rework CookieConsent, use Tag Manager Consent mode

--- src/components/Footer/FooterLinksColumn/FooterLinksColumn.tsx
+++ src/components/Footer/FooterLinksColumn/FooterLinksColumn.tsx
@@ -7,20 +7,34 @@ import styles from './FooterLinksColumn.module.scss';
 interface FooterLinksColumnProps {
   heading: string;
   links: Array<{
+    id?: string;
     title: string;
     href: string;
     rel?: string;
   }>;
+  onClick?: (id: string, event: React.MouseEvent) => void;
 }
 
-export function FooterLinksColumn({ heading, links }: FooterLinksColumnProps): JSX.Element {
+export function FooterLinksColumn({ heading, links, onClick }: FooterLinksColumnProps): JSX.Element {
+  const handleClick = (id?: string) => (event: React.MouseEvent) => {
+    if (id) {
+      onClick?.(id, event);
+    }
+  };
   return (
     <div className={styles.root}>
       <h3 className={styles.root__heading}> {heading} </h3>
       <ul className={styles.root__links}>
-        {links.map(({ title, href, rel }, index) => (
+        {links.map(({ id, title, href, rel }, index) => (
           <li className={styles.root__item} key={index}>
-            <Button variant="linkGray" size="lg" href={href} className={styles.root__link} rel={rel}>
+            <Button
+              variant="linkGray"
+              size="lg"
+              href={href}
+              className={styles.root__link}
+              rel={rel}
+              onClick={handleClick(id)}
+            >
               {title}
             </Button>
           </li>
--- src/containers/CookieConsent/CookieConsent.tsx
+++ src/containers/CookieConsent/CookieConsent.tsx
@@ -1,32 +1,15 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
 
 import { Container } from '../../components/Container/Container';
-import { CookieConsent as CookieConsentBase } from '../../components/CookieConsent/CookieConsent';
+import { CookieConsent as CookieConsentBase, CookieConsentProps } from '../../components/CookieConsent/CookieConsent';
 
 import styles from './CookieConsent.module.scss';
 
-export function CookieConsent(): JSX.Element {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          googleTagManager {
-            trackingId
-          }
-          yandexMetrika {
-            trackingId
-          }
-        }
-      }
-    }
-  `);
-  const { googleTagManager, yandexMetrika } = data.site.siteMetadata;
-
+export function CookieConsent(props: CookieConsentProps): JSX.Element {
   return (
     <div className={styles.root}>
       <Container>
-        <CookieConsentBase googleTagManager={googleTagManager} yandexMetrika={yandexMetrika} />
+        <CookieConsentBase {...props} />
       </Container>
     </div>
   );
